e02a4ff2283046a7d047e4dbb730bb54849a5fc7
Saurabh Sharma <132290433+saurabh-945798@users.noreply.github.com>
2024-01-27 10:11:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:"#main",
+    // markers:true,
+    start:"75% 50%",
+    end:"100% 50%",
+    scrub:5,
+    duration: 1,
+    pin:true,
+  }});
+  tl
+  .to(".img",{
+    width: "100vw",
+    height: "100vh",
+  })
+
+
+
+  var t2 = gsap.timeline({scrollTrigger:{
+    trigger:"#mainA",
+    // markers:true,
+    start:"75% 50%",
+    end:"100% 50%",
+    scrub:5,
+    duration: 1,
+    pin:true,
+  }});
+
+
+  t2
+  .to(".imgA",{
+    width: "100vw",
+    height: "100vh",
+  })
